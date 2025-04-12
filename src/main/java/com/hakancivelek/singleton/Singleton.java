3765e154a98906d2b8ed28bac743b20f8c864bf8
Hakan CİVELEK <hakancivelek5461@gmail.com>
2025-04-12 18:03:09 +0300

    update for practicing!

--- src/main/java/com/hakancivelek/singleton/Singleton.java
+++ src/main/java/com/hakancivelek/singleton/Singleton.java
@@ -1,22 +1,20 @@
 package com.hakancivelek.singleton;
 
 public class Singleton {
-	
-	private static Singleton singleton = new Singleton();
-	
-	private static int count;
-	private String name;
+    private static int counter = 0;
+    private String name;
+    private static Singleton singleton = new Singleton();
 
-	private Singleton() {
-		count++;
-		name = "Singleton" + count;
-	}
+    private Singleton() {
+        counter++;
+        name = "Singleton " + counter;
+    }
 
-	public static Singleton getInstance() {
-		return singleton;
-	}
+    public static Singleton getInstance() {
+        return singleton;
+    }
 
-	public void printName() {
-		System.out.println(name);
-	}
+    public void printName() {
+        System.out.println(name);
+    }
 }
--- src/main/java/com/hakancivelek/singleton/SingletonClient.java
+++ src/main/java/com/hakancivelek/singleton/SingletonClient.java
@@ -2,21 +2,22 @@ package com.hakancivelek.singleton;
 
 public class SingletonClient {
 
-	public static void main(String[] args) {
-		for (int i = 0; i<10; i++){
-			Singleton s = Singleton.getInstance();
-			s.printName();
-		}
-		
-		Singleton s1 = Singleton.getInstance();
-		System.out.println("\nHashcode: " + s1.hashCode());
-		
-		Singleton s2 = Singleton.getInstance();
-		System.out.println("Hashcode: " + s2.hashCode());
-		
-		if(s1 == s2)
-			System.out.println("\nThe same object!");
-		else
-			System.out.println("Different objects!");
-	}
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            Singleton singleton = Singleton.getInstance();
+
+            singleton.printName();
+        }
+
+        Singleton singleton1 = Singleton.getInstance();
+        System.out.println("\nHashcode: " + singleton1.hashCode());
+
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println("\nHashcode: " + singleton2.hashCode());
+
+        if (singleton1 == singleton2)
+            System.out.println("\nThe same object!");
+        else
+            System.out.println("Different objects!");
+    }
 }

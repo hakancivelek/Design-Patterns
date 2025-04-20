31113d2cda94d5b4fd7029b2640f42b7cb94e2c5
Hakan CİVELEK <hakancivelek5461@gmail.com>
2025-04-20 16:55:19 +0300

    update for practicing! (BillPughSingleton)

--- src/main/java/com/hakancivelek/singleton/BillPughSingleton.java
+++ src/main/java/com/hakancivelek/singleton/BillPughSingleton.java
@@ -4,20 +4,20 @@ public class BillPughSingleton {
     private int counter = 0;
     private static String name;
 
-    private static class SingletonHelper{
+    private static class SingletonHelper {
         private static final BillPughSingleton INSTANCE = new BillPughSingleton();
     }
 
-    private BillPughSingleton(){
+    private BillPughSingleton() {
         counter++;
         name = "BillPughSingleton " + counter;
     }
 
-    public static BillPughSingleton getInstance(){
+    public static BillPughSingleton getInstance() {
         return SingletonHelper.INSTANCE;
     }
 
-    public void printName(){
+    public void printName() {
         System.out.println(name);
     }
 }
--- src/main/java/com/hakancivelek/singleton/BillPughSingletonClient.java
+++ src/main/java/com/hakancivelek/singleton/BillPughSingletonClient.java
@@ -2,7 +2,7 @@ package com.hakancivelek.singleton;
 
 public class BillPughSingletonClient {
     public static void main(String[] args) {
-        for(int i = 0; i < 200; i++){
+        for (int i = 0; i < 200; i++) {
             BillPughSingleton singleton = BillPughSingleton.getInstance();
             singleton.printName();
         }
